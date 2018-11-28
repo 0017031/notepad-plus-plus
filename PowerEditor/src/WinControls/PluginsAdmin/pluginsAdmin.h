c52c88ce0cde11320a6e09e2c56879f553f4d90f
baic <baic@SHA1008096.bsci.bossci.com>
2018-11-28 11:55:59 +0800

    * Update pluginsAdmin.h, using _TCHAR and _istdigit()
    	use _TCHAR and _istdigit() to avoid wchar/char mismatch, and possible undefined behavior.
    	https://en.cppreference.com/w/cpp/string/byte/isdigit

--- PowerEditor/src/WinControls/PluginsAdmin/pluginsAdmin.h
+++ PowerEditor/src/WinControls/PluginsAdmin/pluginsAdmin.h
@@ -7,10 +7,10 @@
 // version 2 of the License, or (at your option) any later version.
 //
 // Note that the GPL places important restrictions on "derived works", yet
-// it does not provide a detailed definition of that term.  To avoid      
-// misunderstandings, we consider an application to constitute a          
+// it does not provide a detailed definition of that term.  To avoid
+// misunderstandings, we consider an application to constitute a
 // "derivative work" for the purpose of this license if it does any of the
-// following:                                                             
+// following:
 // 1. Integrates source code from Notepad++.
 // 2. Integrates/includes/aggregates Notepad++ into a proprietary executable
 //    installer, such as those produced by InstallShield.
@@ -50,8 +50,8 @@ struct Version
 	void setVersionFrom(generic_string filePath);
 	generic_string toString();
 	bool isNumber(const generic_string& s) const {
-		return !s.empty() && 
-			find_if(s.begin(), s.end(), [](char c) { return !isdigit(c); }) == s.end();
+		return !s.empty() &&
+			find_if(s.begin(), s.end(), [](_TCHAR c) { return !_istdigit(c); }) == s.end();
 	};
 
 	int compareTo(const Version& v2c) const;
@@ -86,7 +86,7 @@ struct PluginUpdateInfo
 	generic_string _author;
 	generic_string _id;           // Plugin package ID: SHA-256 hash
 	generic_string _repository;
-	bool _isVisible = true;       // if false then it should not be displayed 
+	bool _isVisible = true;       // if false then it should not be displayed
 
 	generic_string describe();
 	PluginUpdateInfo() {};
@@ -99,7 +99,7 @@ struct NppCurrentStatus
 
 	bool _isInProgramFiles;         // true: install/update/remove on "Program files" (ADMIN MODE)
 									// false: install/update/remove on NPP_INST or install on %APPDATA%, update/remove on %APPDATA% & NPP_INST (NORMAL MODE)
-									
+
 	bool _isAppDataPluginsAllowed;  // true: install on %APPDATA%, update / remove on %APPDATA% & "Program files" or NPP_INST
 
 	generic_string _nppInstallPath;
@@ -198,7 +198,7 @@ private :
 
 	TabBar _tab;
 
-	PluginViewList _availableList; // A permanent list, once it's loaded (no removal - only hide or show) 
+	PluginViewList _availableList; // A permanent list, once it's loaded (no removal - only hide or show)
 	PluginViewList _updateList;    // A dynamical list, items are removable
 	PluginViewList _installedList; // A dynamical list, items are removable
 
@@ -218,7 +218,7 @@ private :
 	long searchInDescsFromCurrentSel(generic_string str2search, bool isNextMode) const {
 		return searchFromCurrentSel(str2search, _inDescs, isNextMode);
 	};
-	
+
 	bool loadFromPluginInfos();
 	bool checkUpdates();
 
